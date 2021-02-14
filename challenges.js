80b75b04215104b07f279ce5450e4abc878f5626
Ian McKain <ian@Ians-MacBook-Pro.local>
2021-02-14 17:28:08 -0500

    14

--- challenges.js
+++ challenges.js
@@ -426,9 +426,14 @@ Difficulty: Intermediate
 
 Prompt:
 
-- Write a function named fromPairs that creates an object from an array containing nested arrays.
-- Each nested array will have two elements representing key/value pairs used to create key/value pairs in an object to be returned by the function.
-- If a key appears in multiple pairs, the rightmost pair should overwrite previous the previous entry in the object.
+- Write a function named fromPairs that creates an object from an array containing 
+  nested arrays.
+
+- Each nested array will have two elements representing key/value pairs used 
+  to create key/value pairs in an object to be returned by the function.
+
+- If a key appears in multiple pairs, the rightmost pair should overwrite previous 
+  the previous entry in the object.
 
 Examples:
 
@@ -437,7 +442,13 @@ fromPairs([ ['name', 'Sam"], ['age', 24], ['name', 'Sally'] ]) //=> { name: "Sal
 -----------------------------------------------------------------*/
 // Your solution for 14-fromPairs here:
 
-
+function fromPairs(arr) {
+  let obj = {}
+  arr.forEach(function(keyValue) {
+    obj[keyValue[0]] = keyValue[1]
+  })
+  return obj
+}
 
 
 
