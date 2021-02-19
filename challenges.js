601623a3c70d1ff0be2049b98a91d4581a89439b
imckain <ianmmckain@gmail.com>
2021-02-19 17:51:25 -0500

    finished 19

--- challenges.js
+++ challenges.js
@@ -632,15 +632,18 @@ Difficulty:  Intermediate
 
 Prompt:
 
-- Write a function named flatten that accepts a single array that may contain nested arrays and returns a new "flattened" array.
+- Write a function named flatten that accepts a single array that may contain nested arrays 
+  and returns a new "flattened" array.
 - A flattened array is an array that contains no nested arrays.
 - Arrays maybe nested at any level.
-- If any of the arrays have duplicate values those duplicate values should be present in the returned array.
+- If any of the arrays have duplicate values those duplicate values should be present in the 
+  returned array.
 - The values in the new array should maintain their ordering as shown in the examples below.
 
 Hint:
 
-- This assignment provides an excellent opportunity to use recursion (a function that calls itself).  It can also be solved by using an inner function.
+- This assignment provides an excellent opportunity to use recursion (a function that calls itself).  
+  It can also be solved by using an inner function.
 
 Examples:
 
@@ -652,6 +655,17 @@ flatten( [1, [2, [3, [4]]], 1, 'a', ['b', 'c']] );
 -----------------------------------------------------------------*/
 // Your solution for 19-flatten here:
 
+function flatten(arr) {
+  flatArr = [];
+  arr.forEach(function(val) {
+    if(Array.isArray(val)) {
+      flatArr = flatArr.concat(flatten(val));
+    } else {
+      flatArr.push(val);
+    }
+  })
+  return flatArr;
+};
 
 
 
